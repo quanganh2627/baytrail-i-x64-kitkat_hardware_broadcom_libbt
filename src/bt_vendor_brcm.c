c0531b71975f005b224282cae8ba5ca292ffa4c7
Cedric Poignet <cedric.poignet@intel.com>
2013-06-05 11:42:42 +0200

    [BT] Clean the Baud Rate configuration setting
    BZ: 148375
    
    These code improvements has been initialy delivered by
    Broadcom embedded in a patch fixing a BT/WIFI coexistence issue
    causing a WiFi crash. We extracted the code from it.
    Patch: http://android.intel.com:8080/123806
    
    The BT/WIFI coexistence issue is not necessary anymore, it has
    been removed removed in patch: http://android.intel.com:8080/154676.
    
    Following reference refers to the initial Broadcom WiFi fix:
    Release version:        INTEL-AOSP-4.2.2_r1_WAR_001
    Release date:           Tue Apr 23 19:26:45 CEST 2013
    Broadcom ref:           BCM-AOSP-4.2.2_r1_WAR_001
    
    Change-Id: I089c4b200ce65af3fec7caf68673be5ebb38aa0a
    Category: device enablement
    Domain: CWS.BT-BCM
    Origin: 3rd-party broadcom
    Upstream-Candidate: no, proprietary
    Signed-off-by: Cedric Poignet <cedric.poignet@intel.com>

--- src/bt_vendor_brcm.c
+++ src/bt_vendor_brcm.c
@@ -47,6 +47,7 @@
 ******************************************************************************/
 
 void hw_config_start(void);
+void hw_config_cleanup(void);
 uint8_t hw_lpm_enable(uint8_t turn_on);
 uint32_t hw_lpm_get_idle_timeout(void);
 void hw_lpm_set_wake_state(uint8_t wake_assert);
@@ -243,6 +244,7 @@ static void cleanup( void )
     BTVNDDBG("cleanup");
 
     upio_cleanup();
+    hw_config_cleanup();
 
     bt_vendor_cbacks = NULL;
 }
