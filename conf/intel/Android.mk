51a3f75a3cb2bbc5fc9c1381e72b2dd1128510c3
Aymen Zayet <aymen.zayet@intel.com>
2013-09-10 19:16:30 +0200

    [REVERTME][BT] libbt: take into account _next used in TARGET_PRODUCT.
    BZ: 137499
    
    Change-Id: I39c2a75edb0fe1bef8e14419ad08c26c38b878d1
    Signed-off-by: Aymen Zayet <aymen.zayet@intel.com>
    Reviewed-on: http://android.intel.com:8080/131171
    Reviewed-by: Combes, Patrick <patrick.combes@intel.com>
    Reviewed-by: Gros, Julien <julien.gros@intel.com>
    Reviewed-by: Fiat, Christophe <christophe.fiat@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- conf/intel/Android.mk
+++ conf/intel/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := bt_vendor.conf
 LOCAL_MODULE_TAGS := optional
+ifeq (, $(filter %_next, $(TARGET_PRODUCT)))
 LOCAL_REQUIRED_MODULES := \
 	bt_baylake.conf \
 	bt_byt_m_crb.conf \
@@ -16,6 +17,20 @@ LOCAL_REQUIRED_MODULES := \
 	bt_saltbay_pr0.conf \
 	bt_saltbay_pr1.conf \
 	bt_victoriabay.conf
+else
+LOCAL_REQUIRED_MODULES := \
+	bt_baylake_next.conf \
+	bt_byt_m_crb_next.conf \
+	bt_byt_t_ffrd10_next.conf \
+	bt_byt_t_ffrd8_next.conf \
+	bt_bodegabay_next.conf \
+	bt_ctp7160_next.conf \
+	bt_ctpscalelt_next.conf \
+	bt_redhookbay_next.conf \
+	bt_saltbay_pr0_next.conf \
+	bt_saltbay_pr1_next.conf \
+	bt_victoriabay_next.conf
+endif
 
 include $(BUILD_PHONY_PACKAGE)
 
