8b397204590f0f22cfa16e8e10731d8a4ca22f31
Aymen Zayet <aymen.zayet@intel.com>
2013-09-10 19:16:30 +0200

    [PORT FROM MAIN][BT] libbt: take into account _next used in TARGET_PRODUCT.
    BZ: 137499
    
    Change-Id: I6087d3074de519589351370d35b07ee689591fa4
    Orig-Change-Id: I39c2a75edb0fe1bef8e14419ad08c26c38b878d1
    Category: device enablement
    Domain: CWS.BT-Common
    Origin: internal
    Upstream-Candidate: no, not-aosp
    Signed-off-by: Aymen Zayet <aymen.zayet@intel.com>
    Reviewed-on: http://android.intel.com:8080/131171
    Reviewed-by: Combes, Patrick <patrick.combes@intel.com>
    Reviewed-by: Gros, Julien <julien.gros@intel.com>
    Reviewed-by: Fiat, Christophe <christophe.fiat@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>

--- conf/intel/byt_t_ffrd8/Android.mk
+++ conf/intel/byt_t_ffrd8/Android.mk
@@ -1,7 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq (, $(filter %_next, $(TARGET_PRODUCT)))
 LOCAL_MODULE := bt_$(notdir $(LOCAL_PATH)).conf
+else
+LOCAL_MODULE := bt_$(notdir $(LOCAL_PATH))_next.conf
+endif
 $(call print-vars $(LOCAL_MODULE))
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
