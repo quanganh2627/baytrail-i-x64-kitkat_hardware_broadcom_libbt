26d86d65fcee34dcb0c74d882c0a485ad9b4a291
Jocelyn Falempe <jocelyn.falempe@intel.com>
2013-12-09 11:52:23 +0100

    remove _next from Android.mk
    BZ: 157498
    
    Change-Id: I849622e2052e585ad2d2bf078ab6d013328090d2
    Category: device enablement
    Domain: CWS.BT-Common
    Origin: internal
    Upstream-Candidate: no, not-aosp
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>

--- conf/intel/Android.mk
+++ conf/intel/Android.mk
@@ -1,29 +1,43 @@
 LOCAL_PATH:= $(call my-dir)
 
+# find all BT config files
+ALL_BT_CONFIG := $(subst /,,$(dir $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/*/bt_vendor.conf))))
+
+# $1 is the product_name
+# $2 is the source path
+define bt_copy_conf
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := bt_$(1).conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/bluetooth
+LOCAL_SRC_FILES := $(2)/bt_vendor.conf
+include $(BUILD_PREBUILT)
+
+ALL_BT_CONFIG_MODULE += bt_$(1).conf
+endef
+
+# copy all product bt_vendor.conf to /etc/bluetooth/bt_$(TARGET_PRODUCT).conf
+$(foreach bt_conf,$(ALL_BT_CONFIG),$(eval $(call bt_copy_conf,$(bt_conf),$(bt_conf))))
+
+# if REF_PRODUCT_NAME also copy $(REF_PRODUCT_NAME)/bt_vendor.conf
+# to /etc/bluetootch/bt_$(TARGET_PRODUCT).conf
+# useful for xxxx_next and xxxx_64 target
+ifneq ($(TARGET_PRODUCT),$(REF_PRODUCT_NAME))
+ifneq ($(wildcard $(LOCAL_PATH)/$(REF_PRODUCT_NAME)/bt_vendor.conf),)
+ifeq ($(wildcard $(LOCAL_PATH)/$(TARGET_PRODUCT)/bt_vendor.conf),)
+$(eval $(call bt_copy_conf,$(TARGET_PRODUCT),$(REF_PRODUCT_NAME)))
+endif
+endif
+endif
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := bt_vendor.conf
 LOCAL_MODULE_TAGS := optional
-ifeq (, $(filter %_next, $(TARGET_PRODUCT)))
-LOCAL_REQUIRED_MODULES := \
-	bt_baylake.conf \
-	bt_baylake_edk2.conf \
-	bt_byt_m_crb.conf \
-	bt_byt_t_ffrd8.conf \
-	bt_ctp7160.conf \
-	bt_redhookbay.conf \
-	bt_saltbay.conf \
-	bt_mofd_v0.conf
-else
-LOCAL_REQUIRED_MODULES := \
-	bt_baylake_next.conf \
-	bt_byt_m_crb_next.conf \
-	bt_byt_t_ffrd8_next.conf \
-	bt_ctp7160_next.conf \
-	bt_redhookbay_next.conf \
-	bt_saltbay_next.conf
-endif
+
+LOCAL_REQUIRED_MODULES := $(ALL_BT_CONFIG_MODULE)
 
 include $(BUILD_PHONY_PACKAGE)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
