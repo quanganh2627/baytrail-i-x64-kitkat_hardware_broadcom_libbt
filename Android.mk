df9cf7681294c2d715462c785a6c81f4c6c43a8c
Cedric Poignet <cedric.poignet@intel.com>
2014-11-28 14:22:40 +0100

    [CWS_CELLCOEX] Add a Binder interface to send HCI cmd
    It allows the Cellular Coex service to directly send
    HCI commands for its own needs.
    
    Not compiled if BOARD_USE_CELLULAR_COEX is not defined.
    
    Change-Id: I1d5a4937b6b2c294699154702c62c115f50fbb63
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-4475
    Signed-off-by: Mick CORNUT <mick.cornut@intel.com>
    Signed-off-by: Cedric Poignet <cedric.poignet@intel.com>
    Reviewed-on: https://android.intel.com/307364
    Reviewed-by: Dalleau, Frederic <frederic.dalleau@intel.com>
    Reviewed-by: Bellouard, Jean-marie <jean-marie.bellouard@intel.com>
    Reviewed-by: Odeberg, Hans <hans.odeberg@intel.com>
    Reviewed-by: Svenberg, Stefan <stefan.svenberg@intel.com>
    Tested-by: Svenberg, Stefan <stefan.svenberg@intel.com>

--- Android.mk
+++ Android.mk
@@ -21,6 +21,19 @@ LOCAL_SHARED_LIBRARIES := \
         libcutils \
         liblog
 
+ifeq ($(strip $(BOARD_USE_CELLULAR_COEX)),true)
+    LOCAL_REQUIRED_MODULES += libbtvendorcellcoex-client
+       LOCAL_SRC_FILES += src/hci_service.c
+       LOCAL_CFLAGS += -DUSE_CELLULAR_COEX
+    LOCAL_SHARED_LIBRARIES += \
+               libstlport \
+               libbinder \
+               libutils \
+               libandroid_runtime \
+               liblog \
+               libbtvendorcellcoex-client
+endif
+
 LOCAL_MODULE := libbt-vendor
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
